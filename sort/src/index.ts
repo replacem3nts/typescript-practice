3fb36e3394751c34feaf38162c17a39924ddae1a
replacem3nts <donny.landis@gmail.com>
2020-09-22 13:47:53 -0400

    completes sort project

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,21 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 30, -5, 0, -7]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('ZeBrAstRipes');
-const charSorter = new Sorter(charactersCollection);
-charSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-5);
+linkedList.add(25);
+linkedList.add(16);
+
+linkedList.sort();
+linkedList.print();
