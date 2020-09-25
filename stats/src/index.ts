ea2ca6c7087918ec3f182bfa2361cf0a9adc1d97
replacem3nts <donny.landis@gmail.com>
2020-09-25 15:08:08 -0400

    completes inheritance vs. composition

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,15 +1,17 @@
+import { read } from 'fs';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const reader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(reader);
+matchReader.load();
 
-console.log(reader.data[0][0]);
+// console.log(matchReader.matches);
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
