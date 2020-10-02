cc67b0ac1603d00f0a3f7e69236f9ac84a42c8d9
replacem3nts <donny.landis@gmail.com>
2020-10-02 12:55:16 -0400

    completes login functionality with cookie-session

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) { }
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -9,5 +10,26 @@ router.get('/login', function (req, res) {
 });
 router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    res.send(email + password);
+    if (email &&
+        password &&
+        email === 'hello@gmail.com' &&
+        password === 'password') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n      <div>\n        <div>You are logged in</div>\n        <a href=\"/logout\" >Logout</a>\n      </div>\n      ");
+    }
+    else {
+        res.send("\n        <div>\n          <div>You are not logged in</div>\n          <a href=\"/login\" >Login</a>\n        </div>\n      ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = undefined;
+    res.redirect('/');
 });
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,4 +1,10 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {}
 
 const router = Router();
 
@@ -18,9 +24,42 @@ router.get('/login', (req: Request, res: Response) => {
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  res.send(email + password);
+  if (
+    email &&
+    password &&
+    email === 'hello@gmail.com' &&
+    password === 'password'
+  ) {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout" >Logout</a>
+      </div>
+      `);
+  } else {
+    res.send(`
+        <div>
+          <div>You are not logged in</div>
+          <a href="/login" >Login</a>
+        </div>
+      `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
 });
 
 export { router };
